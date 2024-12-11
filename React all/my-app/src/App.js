c20ba14e67fe7805a27db26c3ca202454cccdde4
boyavamsi123 <vamvamshi56@gmail.com>
2024-12-11 21:27:48 +0530

    check

---
+++ React all/my-app/src/App.js
@@ -0,0 +1,11 @@
+import Message from "./StateEx/Message"
+import Product from "./Product/Product"
+let App = ()=>{
+    
+    return <div>
+             <h1>App Component</h1>
+             <hr/>
+             <Product/>
+           </div>
+}
+export default App
