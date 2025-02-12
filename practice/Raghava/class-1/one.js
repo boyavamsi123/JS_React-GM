22417e227135d0a50cfbc56090b1305af99c0eb4
boyavamsi123 <vamvamshi56@gmail.com>
2025-02-12 19:55:28 +0530

    txt

---
+++ practice/Raghava/class-1/one.js
@@ -0,0 +1,15 @@
+document.write("M Poojitha"+"<br>");
+document.write("K Ruchitha"+"<br>");
+document.write("Ganesh"+"<br>");
+document.write("K Subba Rao"+"<br>");
+document.write("Shaik Roshini"+"<br>");
+document.write("S.Maheen Noor"+"<br>");
+alert("Dont open this website");
+alert("Iam Konde Rajini from USA");
+document.write("k Shilpa"+"<br>");
+document.write("Anwar Basha"+"<br>");
+document.getElementById("meghana").innerHTML="Meghana";
+console.log("Hi iam from Bangalore.....");
+console.log("Jagadheesh Achari");
+console.log("we are all from america...");
+console.log("From UK");
